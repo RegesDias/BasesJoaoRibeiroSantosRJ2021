2ebda798e922be763c3040bb6c33abb37f1aee52
RegesDias <regesfd@gmail.com>
2021-01-28 09:52:20 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+$mysqli = new mysqli("localhost","root","semad@cpd","GrandeJogo");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+$respPost = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
+function retornaNome($id ,$tabela){
+    $sql = "SELECT nome FROM $tabela WHERE id = '$id'";
+    $rnome = $GLOBALS['mysqli']->query($sql);
+    $rn = $rnome->fetch_object();
+    echo $rn->nome;
+}
+function retornaNota($idUser, $idBase){
+  $sql = "SELECT nota FROM notas WHERE idUser = '$idUser' and idBase = '$idBase'";
+  $rnome = $GLOBALS['mysqli']->query($sql);
+  $rn = $rnome->fetch_object();
+  return $rn->nota;
+}
+if($respPost['acao'] == 'encaminhar'){
+  $id = $respPost['id'];
+  $idUser = $_SESSION['idUser'];
+  $dd = $mysqli->query("SELECT * FROM base WHERE id ='$id'");
+  $direcionar = $dd->fetch_object();
+  if($direcionar->status == 'Aberta'){
+    $alterar = $mysqli->query("UPDATE base SET status = 'Fechada', idUser = '$idUser' WHERE id = '$id'");
+    $insertBase = "INSERT INTO baseFeitas(idBase, idUser,ativo) VALUES ($id,$idUser,1)";
+    $ib = $mysqli->query($insertBase);
+    header('Location: '.$direcionar->link);
+  }else{?>
+    <div class="alert alert-warning">
+      <button type="button" class="close" data-dismiss="alert">×</button>
+      <h4>Alerta!</h4>
+      Foi quase... mas infelizmente esta base já foi acessada
+    </div><?php
+  }
+}
+
+if($respPost['acao'] == 'entrar'){
+    $senha  = md5($respPost['passwd']);
+    $chave = $respPost['user'];
+    if ($login = $mysqli->query("SELECT * FROM user WHERE senha = '$senha' AND chave = '$chave'")) {
+      $acesso = $login->fetch_object();
+      $_SESSION['ativo'] = false;
+      $_SESSION['admin'] = false;
+      $_SESSION['chefeBase'] = false;
+      if($acesso->ativo == 1){
+        $_SESSION['idUser'] = $acesso->id;
+        $_SESSION['nome'] = $acesso->nome;
+        $_SESSION['ativo'] = true;
+        if($acesso->admin == 1){
+          $_SESSION['admin'] = true;
+        }
+        if($acesso->chefeBase == 1){
+          $_SESSION['chefeBase'] = true;
+        }
+      }else{?>
+    <div class="alert alert-danger">
+      <button type="button" class="close" data-dismiss="alert">×</button>
+      <h4>Alerta!</h4>
+        Usuário ou senha incorretos...
+    </div><?php
+      }
+    }
+}
+if($respPost['acao'] == 'abrirBase'){
+  $id = $respPost['id'];
+  $idUser = $respPost['idUser'];
+  $nota = $respPost['nota'];
+  $avaliadoPor = $_SESSION['idUser'];
+  if($respPost['status'] == 'Aberta'){
+    $status = 'Fechada';
+  }else{
+    $status = 'Aberta';
+  }
+  $alterar = $mysqli->query("UPDATE base SET status = '$status' WHERE id = '$id'");
+  $insertNota = "INSERT INTO notas(idBase, idUser,nota,avaliadoPor) VALUES ($id,$idUser,$nota,$avaliadoPor )";
+  $in = $mysqli->query($insertNota);
+}
+//testes
+//echo '<br>';
+//print_r($respPost);
+?>
---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta http-equiv=”refresh” content=”5" />
+  <title>Controle de Bases</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+<?php 
+  require_once('config.php');
+?>
+<!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+    <?php
+     if($_SESSION['ativo'] == true){
+        if($_SESSION['admin'] === true){ ?>
+            <a class="navbar-brand" href="#">Bom vindo Chefe <?=$_SESSION['nome']?></a>
+        <?php }else{ ?>
+          <a class="navbar-brand" href="#">Patrulha <?=$_SESSION['nome']?> Alerta!</a>
+        <?php }
+      }?>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#"  data-toggle="modal" data-target="#modalExemplo">Entrar</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Page Content -->
+  <div class="container">
+
+<!-- Modal -->
+<div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form method="post">
+        <div class="modal-body">
+          <label>Usuário</label>
+          <input type="text" name='user' class="form-control"  placeholder="usuario">
+          <label>Senha</label>
+          <input type="password" name='passwd' class="form-control"  placeholder="senha">
+          <input type="hidden" name='acao' value='entrar'>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+          <input type="submit" value='Entrar' class="btn btn-primary">
+        </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+    <div class="row">
+      <div class="col-lg-12">
+
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="img/joaoRibeiro.jpeg" alt="First slide">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+        <?php
+          if($_SESSION['ativo'] == true){
+            if($_SESSION['chefeBase'] == true){
+              $idUser = $_SESSION['idUser'];
+              $basesql = "SELECT * FROM base WHERE ResposavelBase = '$idUser' ";
+            }else{
+              $basesql = "SELECT * FROM base";
+            }
+            if ($result = $mysqli->query($basesql)) {
+              while ($base = $result->fetch_object()) {?>
+                  <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-100">
+                      <a href="#"><img height="700" width="50" class="card-img-top img-fluid border-radius img-thumbnail" src="img/<?=$base->img?>" alt=""></a>
+                        <div class="card-footer"><?php
+                          $idUser = $_SESSION['idUser'];
+                          $feita = $mysqli->query("SELECT * FROM baseFeitas WHERE idBase = '$base->id' AND idUser = '$idUser'");
+                          $baseJaFeita = $feita->fetch_object();
+                          if($baseJaFeita->ativo == 1){
+                            echo "<button class='btn btn-large btn-block '  disabled href='#'>Nota ".retornaNota($_SESSION['idUser'],$base->id)."</button>";
+                          }else{
+                                  if(($base->status === 'Aberta') and ($_SESSION['admin'] != true)){?>
+                                    <form method="post">
+                                      <input type='hidden' name='acao' value='encaminhar'>
+                                      <input type='hidden' name='status' value='<?=$base->status?>'>
+                                      <input type='hidden' name='id' value='<?=$base->id?>'>
+                                      <input type='submit' value='Aberta!' class="btn btn-large btn-block btn-success">
+                                    </form><?php
+                                  }
+                                  if(($base->status === 'Aberta') and ($_SESSION['admin'] == true)){
+                                    echo "<button class='btn btn-large btn-block btn-success'  disabled href='#'>Vazia</button>";
+                                  }
+                                  if(($_SESSION['admin'] === true) and ($base->status != 'Aberta')){
+                                      ?>
+                                      <form method="post">
+                                        <label>Avaliar Patrulha <?=retornaNome($base->idUser ,'user')?></label>
+                                        <input type="text" name='nota' class="form-control"  placeholder="Nota">
+                                        <input type='hidden' name='acao' value='abrirBase'>
+                                        <input type='hidden' name='status' value='<?=$base->status?>'>
+                                        <input type='hidden' name='idUser' value='<?=$base->idUser?>'>
+                                        <input type='hidden' name='id' value='<?=$base->id?>'>
+                                        <label></label>
+                                        <input type='submit' value='Abrir e Avaliar' class="btn btn-large btn-block btn-primary">
+                                      </form><?php
+                                  }
+                                  if(($base->status === 'Fechada')and ($_SESSION['admin'] != true)){
+                                    echo "<button class='btn btn-large btn-block btn-danger'  disabled href='#'>Fechada</button>";
+                                  }
+                                  
+                           }?>
+                        </div>
+                    </div>
+                  </div><?php 
+              }
+            }
+          }
+          ?>
+
+
+
+  
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Inicio do evento dia 00/00/0000 as 00:00</p>
+      <p class="m-0 text-center text-white">Enceramento dia 00/00/0000 as 00:00</p>
+      <p class="m-0 text-center text-white">Duvidas entrar em contato com ddd@ddd.com</p>
+      <p class="m-0 text-center text-white">Inscreva-se em www.lugar.com.br</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script>
+  setTimeout(function(){
+    window.location.reload(1);
+  }, 50000);
+</script>
+</body>
+
+</html>
