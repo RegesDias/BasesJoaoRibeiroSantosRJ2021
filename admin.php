8751e5952698f87324d58a8a670e8bdfe1696729
RegesDias <regesfd@gmail.com>
2021-02-13 13:17:52 -0300

    Passos-GitHub

---
+++ admin.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php require_once("incl/head.php"); ?>
+<body>
+<?php 
+    require_once('config.php');
+    require_once('incl/nav.php');
+  ?>      
+    <div class="container">
+                 
+    </div>
+
+</body>
+</html>
