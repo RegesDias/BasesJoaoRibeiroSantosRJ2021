2edeb1d954fe3fc976535ee7743fc3bf05d29567
RegesDias <71077410+RegesDias@users.noreply.github.com>
2021-03-23 17:19:36 -0300

    Update config.php

--- incl/config.php
+++ incl/config.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-//$server = "localhost";
-$server = "187.45.196.218";
-
-$mysqli = new mysqli($server ,"basesgrandejog","ondeumvai@99T","basesgrandejog");
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
